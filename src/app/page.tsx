c598d6caf0222f18244f4f6e8c6abce3354f8c7f
Ranu singh <ranu2004singh@gmail.com>
2025-07-25 07:02:09 +0000

    ReactDOM.useFormState has been renamed to React.useActionState. Please u

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-
 'use client';
 
-import { useFormState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import {
@@ -48,7 +48,7 @@ function SubmitButton() {
 }
 
 export default function LoginPage() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+  const [errorMessage, dispatch] = useActionState(authenticate, undefined);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
